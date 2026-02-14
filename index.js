fa27e952c5b1266a541e652355b8bc3672ae5a19
coltin14 <coltpayton34@yahoo.com>
2026-02-14 13:28:28 -0600

    Testing

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function rollDice() {
-  document.getElementById("d1").value =
-    Math.floor(Math.random() * 6) + 1;
+    document.getElementById("d1").value =
+        Math.floor(Math.random() * 6) + 1;
 
-  document.getElementById("d2").value =
-    Math.floor(Math.random() * 6) + 1;
+    document.getElementById("d2").value =
+        Math.floor(Math.random() * 6) + 1;
+            
+    document.getElementById("d3").value =
+        Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("d4").value =
+        Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("d5").value =
+        Math.floor(Math.random() * 6) + 1;
+
 }
